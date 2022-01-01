d1f615cba1e09442be5c893053fbf00f80981269
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-01-01 16:16:01 +0900

    달랑 스트링만 반환하는 컨트롤러 추가 - @ResponseBody

--- src/main/java/com/hello/hellospring/controller/HelloController.java
+++ src/main/java/com/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,12 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    // 달랑 스트링만 반환하기
+    @GetMapping("hello-juststring")
+    @ResponseBody
+    public String helloJustString(@RequestParam("name") String name){
+        return "hello " + name; // "hello seoyeon
     }
 }
