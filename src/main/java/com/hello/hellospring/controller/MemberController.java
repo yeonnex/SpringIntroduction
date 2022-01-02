fb6543c248cc00e3fd2df4909c22be9e4e613217
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-01-02 16:56:32 +0900

    회원 관리 예제 - 웹MVC 개발 (회원 등록)

--- src/main/java/com/hello/hellospring/controller/MemberController.java
+++ src/main/java/com/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.hello.hellospring.controller;
 
+import com.hello.hellospring.domain.Member;
 import com.hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -10,5 +13,19 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService){
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
     }
 }
