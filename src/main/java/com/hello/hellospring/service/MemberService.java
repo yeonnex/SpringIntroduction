46a12973dba498b8b11c8c7ce08cbf92999174d4
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-01-02 11:29:35 +0900

    MemberService 리팩토링 - validateDuplicateMember(member); // 중복회원 검증

--- src/main/java/com/hello/hellospring/service/MemberService.java
+++ src/main/java/com/hello/hellospring/service/MemberService.java
@@ -15,13 +15,15 @@ public class MemberService {
      * 회원 가입
      */
     public Long join(Member member){
-        // 비즈니스 로직으로 정한 것 중에,
-        // 같은 이름이 있는 중복 회원 X
+        validateDuplicateMember(member); // 중복회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
         Optional<Member> result = memberRepository.findByName(member.getName());
         result.ifPresent(m ->{
             throw new IllegalStateException("이미 존재하는 회원입니다");
         });
-        memberRepository.save(member);
-        return member.getId();
     }
 }
