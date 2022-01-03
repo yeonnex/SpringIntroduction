69e3fbb114b84bcdf1dfe3b25feaab3615122682
yeonnex <82166132+yeonnex@users.noreply.github.com>
2022-01-03 15:19:55 +0900

    스프링 DB 접근 기술 - 순수 JDBC - build.gradle 파일에 jdbc, h2데이터베이스 관련 라이브러리 추가 - application.properties 파일에 경로 추가 - repository/JdbcMemberRepository.java 로 구현체 - SpringConfig.java 에서 스프링 설정변경   - return new JdbcMemberRepository(dataSource);

--- src/main/java/com/hello/hellospring/SpringConfig.java
+++ src/main/java/com/hello/hellospring/SpringConfig.java
@@ -1,14 +1,23 @@
 package com.hello.hellospring;
 
+import com.hello.hellospring.repository.JdbcMemberRepository;
 import com.hello.hellospring.repository.MemberRepository;
 import com.hello.hellospring.repository.MemoryMemberRepository;
 import com.hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private DataSource dataSource;
 
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -16,7 +25,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 
 }
